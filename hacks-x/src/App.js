8c1eb5feb1fb5ca392924f7c91d20238afac71e6
anjalidevarapalli <ald59976@uga.edu>
2025-02-08 00:39:20 -0500

    edited app.js

--- hacks-x/src/App.js
+++ hacks-x/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      ) : (
+        <div>
+          <h2>Hello, {user.name}!</h2>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
