4d561a300fb710d7d11448cd536d903855e7d57f
Keerthur11 <kr36621@uga.edu>
2025-02-08 10:56:52 -0500

    frontendd

--- hacks-x/src/contents/Home.js
+++ hacks-x/src/contents/Home.js
@@ -1,24 +1,68 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
 const Home = () => {
   return (
     <div className="home-container">
+      {/* Title Section */}
       <div className="title">
         Lyric Analyzer
       </div>
 
-      {/* GIF Image */}
-      <div className="gif-container">
-        <img 
-          src="/assets/lottie-animation.gif" 
-          alt="Animation" 
-          width="100%" 
-          height="auto"
-        />
-      </div>
+      {/* Featured Albums Section */}
+      <section className="featured-albums">
+        <h2>Featured Albums</h2>
+        <div className="albums">
+          <div className="album">
+            <img src="/assets/album1.jpg" alt="Album 1" />
+            <h3>Album Name 1</h3>
+            <p>Artist Name</p>
+          </div>
+          <div className="album">
+            <img src="/assets/album2.jpg" alt="Album 2" />
+            <h3>Album Name 2</h3>
+            <p>Artist Name</p>
+          </div>
+          <div className="album">
+            <img src="/assets/album3.jpg" alt="Album 3" />
+            <h3>Album Name 3</h3>
+            <p>Artist Name</p>
+          </div>
+          <div className="album">
+            <img src="/assets/album4.jpg" alt="Album 4" />
+            <h3>Album Name 4</h3>
+            <p>Artist Name</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Popular Artists Section */}
+      <section className="artists">
+        <h2>Popular Artists</h2>
+        <div className="artist-cards">
+          <div className="artist-card">
+            <img src="/assets/artist1.jpg" alt="Artist 1" />
+            <h3>Artist Name 1</h3>
+          </div>
+          <div className="artist-card">
+            <img src="/assets/artist2.jpg" alt="Artist 2" />
+            <h3>Artist Name 2</h3>
+          </div>
+          <div className="artist-card">
+            <img src="/assets/artist3.jpg" alt="Artist 3" />
+            <h3>Artist Name 3</h3>
+          </div>
+          <div className="artist-card">
+            <img src="/assets/artist4.jpg" alt="Artist 4" />
+            <h3>Artist Name 4</h3>
+          </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="footer">
+        <p>Music Stream App © 2025</p>
+      </footer>
     </div>
   );
 };
